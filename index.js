fa4f2c461322faf642b9a13b8926ed631e936691
Sparkkk <89287792+Hsusparkkk@users.noreply.github.com>
2024-02-17 23:07:52 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+const body = document.querySelector("body")
+let coElem = document.querySelector("#coElem")
+const loopArea = document.querySelector("#loopArea")
+// let a = 1;
+
+
+let v = 500/1000
+let time = window.innerWidth+coElem.offsetWidth/v
+let reGenrT = coElem.offsetWidth/v
+let elemWidth = coElem.offsetWidth
+
+
+
+
+function apr(){
+    // loopArea.appendChild(cloned)
+    coElem.style.animation = "runMarq "+time+"ms forwards linear"
+    reGenrT = elemWidth/v
+    let cloned = coElem.cloneNode(true)
+    loopArea.appendChild(cloned)
+    
+    setTimeout(() => {
+        loopArea.removeChild(cloned)
+    }, time+1000);
+
+}
+
+// main
+
+loopArea.removeChild(coElem)
+apr()
+setInterval(() => {
+    apr()
+    
+}, reGenrT);
+
+
+
+
+
+
+
