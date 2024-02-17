054c19df9365f78814dc01521d40c7cd2f149268
Sparkkk <89287792+Hsusparkkk@users.noreply.github.com>
2024-02-17 23:21:20 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -35,6 +35,11 @@ setInterval(() => {
 }, reGenrT);
 
 
+setInterval(()=>{
+    body.style.backgroundColor = "rgb("+Math.floor(Math.random()*225)+","+Math.floor(Math.random()*225)+","+Math.floor(Math.random()*225)+")"
+},Math.floor(Math.random()*v*1000))
+
+
 
 
 
