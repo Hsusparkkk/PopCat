2fcfc22f8a652679815cbb5ba0678fa38c2b4e18
Sparkkk <89287792+Hsusparkkk@users.noreply.github.com>
2024-02-19 19:01:55 +0800

    Update index.js
    Same as above

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ setInterval(() => {
 
 setInterval(()=>{
     body.style.backgroundColor = "rgb("+Math.floor(Math.random()*225)+","+Math.floor(Math.random()*225)+","+Math.floor(Math.random()*225)+")"
-},Math.floor(Math.random()*v*750))
+},Math.floor(Math.random()*v*450))
 
 
 
